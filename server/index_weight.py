2632d4018ef6425226694b04942ddf9448d5ec7b
klq26-Ryzen2600 <klq26@163.com>
2020-08-16 15:52:29 +0800

    成功把多指数的申万 28 行业整合到一张 dataframe 表。原因有2：
    1. groupby 通过两个字段：sw1_code, sw1_name 后，df 变成了 pd.MultiIndex，这样和 pd.read_csv 并指定 index_col = 0 读出来的 df 是不一样的。后者的索引是一维索引，pd.Index，这一点和测试代码有区别
    2. sort_index 的时候，发现 groupby 返回的 index 是字符串类型，本地的 index 是 int64 类型，需要 index.astype('int64') 否则 sort 的时候会报错，即便不报错，sort 结果也是不对的。

--- server/index_weight.py
+++ server/index_weight.py
@@ -19,7 +19,7 @@ class index_weight:
 
     def __init__(self):
         self.folder = os.path.abspath(os.path.dirname(__file__))
-        self.future_industry_df = pd.read_csv('最新申万行业分类.csv', sep='\t', index_col=0)
+        self.future_industry_df = pd.read_csv(os.path.join(self.folder, '最新申万行业分类.csv'), sep='\t', index_col=0)
         pass
 
     def get_index_stock_industry(self, name, df):
@@ -34,11 +34,14 @@ class index_weight:
             df['sw1_name'] = industry_df['sw1_name']
             # 奇怪的问题，如果不加 apply，weight 后面多很多位无效数字
             df['weight'] = df.weight.apply(lambda x : round(x, 4))
+            df['sw1_name'] = df.sw1_name.apply(lambda x : x.replace('I',''))
         return df
 
     def get_index_weights(self):
-        index_weight_list_df = pd.read_csv('index_weight_list.csv',sep='\t',index_col=0)
+        index_weight_list_df = pd.read_csv(os.path.join(self.folder, 'index_weight_list.csv'),sep='\t',index_col=0)
         idx = 1
+        # x 轴：指数 y 轴：申万行业
+        industry_index_df = pd.read_csv(os.path.join(self.folder, 'sw_industry_list.csv'), sep='\t', index_col=0)
         for x in index_weight_list_df.itertuples():
             # 1. 获取指数权重
             index_weight_df = get_index_weights(x.code)
@@ -48,19 +51,35 @@ class index_weight:
             order = ['display_name', 'date', 'weight']
             index_weight_df = index_weight_df[order]
             index_weight_df = index_weight_df.sort_index()
-            # index_weight_df.to_csv('index_weights/{0}_{1}_{2}.csv'.format(idx, x.code, x.name), sep='\t',encoding='utf-8')
-            # index_weight_df = pd.read_csv('index_weights/{0}_{1}_{2}.csv'.format(idx, x.code, x.name), sep='\t', index_col=0)
             
             # 2. 给指数成分股补充申万一级行业数据
             index_industry_weight_df = self.get_index_stock_industry(x.name, index_weight_df)
-            index_industry_weight_df.to_csv('index_weights/{0}_{1}_{2}.csv'.format(idx, x.code, x.name), sep='\t',encoding='utf-8')
+            index_industry_weight_df.to_csv(os.path.join(self.folder, 'index_weights/{0}_{1}_{2}.csv'.format(str(idx).zfill(2), x.code, x.name)), sep='\t',encoding='utf-8')
             
             # 3. 按行业整合成分股占比
             index_grouped = index_industry_weight_df.groupby(['sw1_code','sw1_name']).sum()
             index_grouped['weight'] = index_grouped.weight.apply(lambda x : round(x, 4))
-            index_grouped.to_csv('index_weights/{0}_{1}_{2}_by_sw_industry.csv'.format(idx, x.code, x.name), sep='\t',encoding='utf-8')
+            index_grouped.to_csv(os.path.join(self.folder, 'index_weights/{0}_{1}_{2}_by_sw_industry.csv'.format(str(idx).zfill(2), x.code, x.name)), sep='\t',encoding='utf-8')
+            # 这里因为 groupby 的关系，把 sw1_code 和 sw1_name 组成了 multiIndex，必须重置 index 并只设定 sw1_code 为 index，否则下面的 isin 会很麻烦（需要判断 name.isin(index.levels[0])）
+            index_grouped = index_grouped.reset_index(drop=False)
+            index_grouped = index_grouped.set_index("sw1_code", drop=True)
+            # groupby 之后的 index 类型是 字符串，后面无法使用 sort index 功能
+            index_grouped.index = index_grouped.index.astype('int64')
+
+            # 4. 把多指数汇总至同一张行业二维表
+            non_industry_df = industry_index_df[~(industry_index_df.sw1_name.isin(index_grouped.sw1_name))]
+            non_industry_df.loc[:, 'weight'] = [0.000 for x in range(len(non_industry_df))]
+            print(non_industry_df.dtypes)
+            index_full_df = pd.concat([index_grouped, non_industry_df])
+            index_full_df['weight'] = index_full_df.weight.astype('float64')
+            print(index_full_df.index)
+            index_full_df = index_full_df.sort_index()
+            industry_index_df[x.name] = index_full_df.weight
             idx += 1
             # break
+        # 把 sw1_name 也挪成 index，从列里删除
+        industry_index_df = industry_index_df.set_index("sw1_name", drop=True, append=True)
+        industry_index_df.to_csv(os.path.join(self.folder, 'index_weights/all_index_sw_weights.csv'), sep='\t',encoding='utf-8')
         pass
 
 if __name__ == "__main__":
