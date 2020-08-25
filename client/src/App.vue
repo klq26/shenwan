8f87ed6dc8979315eb3db7f886faaaa4e3d34833
klq26-Mac mini <klq26@163.com>
2020-08-25 16:37:35 +0800

    增加申万行业历史估值位的页面。（宽度好像略有点宽）

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
+    <ul>
+      <li v-for="(tab, index) in tabs" @click="toggle(index)" :class="{active: active == index}" :key=index>
+        {{tab.type}}
+      </li>
+    </ul>
     <div style="display:flex; justify-content:center; color:#FFF; margin:4px; width:100%;font-size: 0.33rem;">{{datetime}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;距下次更新约 {{myCountdown}} 秒</div>
     <!-- 数据区 -->
-    <IndexValueComponent :swIndustryValues="swIndustryValues" :swStyleValues="swStyleValues"/>
-    <!-- <div style="background-color:#000; width:10rem;" v-for="item in swIndustryValues" :key="item.index">
-      <div style="color:#FFF; width:10rem;">{{item.name}}</div>
-    </div> -->
+    <IndexValueComponent :swIndustryValues="swIndustryValues" :swStyleValues="swStyleValues" v-show="active == 0"/>
+    <!-- 估值区 -->
+    <IndexEvalComponent :swIndustryEvals="swIndustryEvals" :swUltimates="swUltimates" v-show="active == 1"/>
   </div>
 </template>
 
 <script>
 
 import IndexValueComponent from './components/IndexValueComponent'
+import IndexEvalComponent from './components/IndexEvalComponent'
 
 import axios from 'axios'
 import Vue from 'vue'
@@ -25,16 +30,27 @@ var serverIp = process.env.API_ROOT
 export default {
   name: 'App',
   components: {
-    IndexValueComponent
+    IndexValueComponent,
+    IndexEvalComponent
   },
   data () {
     return {
       datetime: '',
       refreshInterval: 90,
       myCountdown: 0,
+      active: 0,
+      tabs: [
+        {
+          type: '申万指数'
+        },
+        {
+          type: '申万估值'
+        }
+      ],
       swIndustryValues: [],
       swStyleValues: [],
-      swIndustryEvals: []
+      swIndustryEvals: [],
+      swUltimates: []
     }
   },
   methods: {
@@ -55,6 +71,9 @@ export default {
     prefixInteger (num, length) {
       return (Array(length).join('0') + num).slice(-length)
     },
+    toggle (i) {
+      this.active = i
+    },
     getSWIndexValues () {
       var that = this
       axios.get(serverIp + 'shenwan/api/index_value').then(function (response) {
@@ -65,45 +84,56 @@ export default {
 
         that.myCountdown = that.refreshInterval
       })
+    },
+    getSWIndexEvals () {
+      var that = this
+      axios.get(serverIp + 'shenwan/api/index_eval').then(function (response) {
+        // 申万行业估值数据
+        that.swIndustryEvals = response.data.data.eval
+        // 申万全市场历史大顶大底数据
+        that.swUltimates = response.data.data.ultimate
+      })
     }
   },
   created: function () {
-    // var that = this
-    // document.onkeyup = function (e) {
-    //   // console.log(e.keyCode)
-    //   var val = that.active
-    //   // 事件对象兼容
-    //   let e1 = e || event || window.event
-    //   // 键盘按键判断:左箭头-37;上箭头-38；右箭头-39;下箭头-40
-    //   if (e1 && e1.keyCode === 37) {
-    //     // console.log('左')
-    //     if (val > 0) {
-    //       val -= 1
-    //     } else {
-    //       val = 1
-    //     }
-    //     that.toggle(val)
-    //   } else if (e1 && e1.keyCode === 38) {
-    //     // console.log('上')
-    //   } else if (e1 && e1.keyCode === 39) {
-    //     // console.log('右')
-    //     if (val < 1) {
-    //       val += 1
-    //     } else {
-    //       val = 0
-    //     }
-    //     that.toggle(val)
-    //   } else if (e1 && e1.keyCode === 40) {
-    //     // console.log('下')
-    //   } else if (e1 && e1.keyCode === 32) {
-    //     // console.log('空格')
-    //   }
-    // }
+    var that = this
+    document.onkeyup = function (e) {
+      // console.log(e.keyCode)
+      var val = that.active
+      // 事件对象兼容
+      let e1 = e || event || window.event
+      // 键盘按键判断:左箭头-37;上箭头-38；右箭头-39;下箭头-40
+      if (e1 && e1.keyCode === 37) {
+        // console.log('左')
+        if (val > 0) {
+          val -= 1
+        } else {
+          val = 1
+        }
+        that.toggle(val)
+      } else if (e1 && e1.keyCode === 38) {
+        // console.log('上')
+      } else if (e1 && e1.keyCode === 39) {
+        // console.log('右')
+        if (val < 1) {
+          val += 1
+        } else {
+          val = 0
+        }
+        that.toggle(val)
+      } else if (e1 && e1.keyCode === 40) {
+        // console.log('下')
+      } else if (e1 && e1.keyCode === 32) {
+        // console.log('空格')
+      }
+    }
     this.myCountdown = this.refreshInterval
     this.updateTime()
     setInterval(this.updateTime, 1 * 1000)
     // 获取指数点位
     this.getSWIndexValues()
+    // 获取指数估值
+    this.getSWIndexEvals()
     // // 给首次估值一个 500 毫秒的延迟执行，防止后者先回来，没有更新页面
     // setTimeout(() => {
     //   that.familyEstimate()
@@ -119,6 +149,28 @@ export default {
   width: 10rem;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+ul{
+  width:10rem;
+  display:flex;
+}
+
+ul li{
+  width:5rem;
+  height:1rem;
+  background: #F0F0F0;
+  display: inline-flex;
+  border-right:1px solid #333333;
+  justify-content: center;
+  align-items: center;
+  cursor:pointer;
+  font-size: 0.4rem;
+}
+
+ul li.active{
+  background-color: rgb(51,153,254);
+  font-weight: bold;
 }
 
 </style>
---
+++ client/src/components/IndexEvalComponent.vue
@@ -0,0 +1,252 @@
+<template>
+  <div class="container" @click="changeShowType()">
+    <div class="section-title">申万行业估值</div>
+    <!-- 标题 -->
+    <div class="sw-industry-container">
+      <div class="index-cell">
+        <p class="index-name">名称</p>
+        <p class="index-name">代码</p>
+        <p class="index-name">股个数</p>
+        <!-- 变化列 1 -->
+        <p class="index-name" v-if="showType==0">PE&#60;0</p>
+        <p class="index-name" v-else-if="showType==1">PE_3Y</p>
+        <p class="index-name" v-else>PB_3Y</p>
+        <!-- 变化列 2 -->
+        <p class="index-name" v-if="showType==0">PE&#62;125</p>
+        <p class="index-name" v-else-if="showType==1">PE_5Y</p>
+        <p class="index-name" v-else>PB_5Y</p>
+        <!-- 变化列 3 -->
+        <p class="index-name" v-if="showType==0">PE</p>
+        <p class="index-name" v-else-if="showType==1">PE_10Y</p>
+        <p class="index-name" v-else>PB_10Y</p>
+        <!-- 变化列 4 -->
+        <p class="index-name" v-if="showType==0">PB</p>
+        <p class="index-name" v-else-if="showType==1">PE_ALL</p>
+        <p class="index-name" v-else>PB_ALL</p>
+      </div>
+    </div>
+    <!-- 数据 -->
+    <div class="sw-industry-container">
+      <div class="index-cell" v-for="(item, i) in swIndustryEvals" :key="item.index" >
+        <p class="index-name" :class="swIndustryIndexColor(item, i)">{{item.sw1_name}}</p>
+        <p class="index-name" :class="swIndustryIndexColor(item, i)">{{item.sw1_code}}</p>
+        <p class="index-value" :style="evalRankBackground(item.total_count / allMarket.total_count == 1 ? 0 : item.total_count / allMarket.total_count)">{{item.total_count}}</p>
+        <!-- 变化列 1 -->
+        <p class="index-value" v-if="showType==0" :style="evalRankBackground(item.pe_negative_count / allMarket.pe_negative_count == 1 ? 0 : item.pe_negative_count / allMarket.pe_negative_count)">{{item.pe_negative_count}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pe_percentile_3y)/100)" v-else-if="showType==1">{{parseFloat(item.pe_percentile_3y).toFixed(1)+'%'}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pb_percentile_3y)/100)" v-else>{{parseFloat(item.pb_percentile_3y).toFixed(1)+'%'}}</p>
+        <!-- 变化列 2 -->
+        <p class="index-value" v-if="showType==0" :style="evalRankBackground(item.pe_over_125_count / allMarket.pe_over_125_count == 1 ? 0 : item.pe_over_125_count / allMarket.pe_over_125_count)">{{item.pe_over_125_count}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pe_percentile_5y)/100)" v-else-if="showType==1">{{parseFloat(item.pe_percentile_5y).toFixed(1)+'%'}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pb_percentile_5y)/100)" v-else>{{parseFloat(item.pb_percentile_5y).toFixed(1)+'%'}}</p>
+        <!-- 变化列 3 -->
+        <p class="index-value" v-if="showType==0">{{item.pe.toFixed(2)}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pb_percentile_10y)/100)" v-else-if="showType==1">{{parseFloat(item.pe_percentile_10y).toFixed(1)+'%'}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pb_percentile_10y)/100)" v-else>{{parseFloat(item.pb_percentile_10y).toFixed(1)+'%'}}</p>
+        <!-- 变化列 4 -->
+        <p class="index-value" v-if="showType==0">{{item.pb.toFixed(2)}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pe_percentile)/100)" v-else-if="showType==1">{{parseFloat(item.pe_percentile).toFixed(1)+'%'}}</p>
+        <p class="index-value" :style="evalRankBackground(parseFloat(item.pb_percentile)/100)" v-else>{{parseFloat(item.pb_percentile).toFixed(1)+'%'}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexValueComponent',
+  props: ['swIndustryEvals', 'swUltimates'],
+  methods: {
+    evalRankBackground (percentile) {
+      if (percentile === 0.0) {
+        return 'background-color:#333'
+      }
+      var style = ''
+      if (percentile <= 0.4) {
+        style = 'background: linear-gradient(90deg, #0fae9d ' + percentile * 100 + '%,#333333 ' + (percentile + 0.1) * 100 + '%);'
+        return style
+      } else if (percentile > 0.4 && percentile <= 0.8) {
+        style = 'background: linear-gradient(90deg, #e5a43a ' + percentile * 100 + '%,#333333 ' + (percentile + 0.1) * 100 + '%);'
+        return style
+      } else if (percentile > 0.8) {
+        if (1 - percentile < 0.1) {
+          var tailRate = 1 - percentile
+          style = 'background: linear-gradient(90deg, #fe5a4b ' + percentile * 100 + '%,#333333 ' + (percentile + tailRate) * 100 + '%);'
+        } else {
+          style = 'background: linear-gradient(90deg, #fe5a4b ' + percentile * 100 + '%,#333333 ' + (percentile + 0.1) * 100 + '%);'
+        }
+        return style
+      } else {
+        return 'background-color: #333'
+      }
+    },
+    swIndustryIndexColor (item, index) {
+      var bgClass = 'index-name-color'
+      if ([0].indexOf(index) > -1) {
+        return bgClass + 0
+      } else if ([1, 2, 3, 4].indexOf(index) > -1) {
+        return bgClass + 1
+      } else if ([5, 6, 7, 8].indexOf(index) > -1) {
+        return bgClass + 2
+      } else if ([9, 10, 11, 12].indexOf(index) > -1) {
+        return bgClass + 3
+      } else if ([13, 14, 15, 16].indexOf(index) > -1) {
+        return bgClass + 4
+      } else if ([17, 18, 19, 20].indexOf(index) > -1) {
+        return bgClass + 5
+      } else if ([21, 22, 23, 24].indexOf(index) > -1) {
+        return bgClass + 6
+      } else if ([25, 26, 27, 28].indexOf(index) > -1) {
+        return bgClass + 7
+      } else {
+        return bgClass + 0
+      }
+    },
+    changeShowType () {
+      if (this.showType < 2) {
+        this.showType += 1
+      } else {
+        this.showType = 0
+      }
+    //   console.log(this.showType)
+    }
+  },
+  created: function () {
+    this.showType = 0
+  },
+  watch: {
+    swIndustryValues: {
+      handler (newValue, oldValue) {
+        if (newValue !== 'undefined') {
+          // this.allMarket = this.mySWIndustryEvals[0]
+          // console.log(this.allMarket)
+        }
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+  data () {
+    return {
+      showType: 0,
+      mySWIndustryEvals: this.swIndustryEvals,
+      mySWUltimates: this.swUltimates,
+      allMarket: {}
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.container {
+  display: flex;
+  flex-wrap:wrap;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.sw-industry-container {
+  display: flex;
+  flex-wrap:wrap;
+  align-items: left;
+  justify-content: left;
+  width: 100%;
+  height: 100%;
+}
+
+.index-cell {
+  display: flex;
+  width: 5rem;
+  justify-content:flex-start;
+  /* justify-content:flex-start; */
+  align-items: center;
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  background-color: #000000;
+}
+
+.section-title {
+  display: flex;
+  flex-wrap:wrap;
+  justify-content: center;
+  font-size: 0.33rem;
+  width: 100%;
+  height: 0.5rem;
+  margin: 0px;
+  padding: 0px;
+  color:#FFFFFF;
+  background-color: #333333;
+}
+
+.index-name {
+    /* 解决纯数字时偏上的问题 */
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  margin: 1px;
+  padding: 1px;
+  width: 1.5rem;
+  height: 0.5rem;
+  font-size: 0.33rem;
+  color:#FFFFFF;
+  background-color: #333333;
+}
+
+.index-value {
+    /* 解决纯数字时偏上的问题 */
+  display: flex;
+  justify-content:flex-end;
+  align-items: center;
+  margin: 1px;
+  padding: 1px;
+  width: 1.5rem;
+  height: 0.5rem;
+  font-size: 0.33rem;
+  /* text-align: center; */
+  color:#FFFFFF;
+  background-color: #333333;
+}
+
+/* 指数名称背景色 */
+.index-name-color0 {
+  color:#FFFFFF;
+  background-color: #333333;
+}
+.index-name-color1 {
+  color:#333333;
+  background-color: #50C2F9;
+}
+.index-name-color2 {
+  color:#333333;
+  background-color: #BBEDA8;
+}
+.index-name-color3 {
+  color:#333333;
+  background-color: #FFC751;
+}
+.index-name-color4 {
+  color:#333333;
+  background-color: #FF7C9E;
+}
+.index-name-color5 {
+  color:#333333;
+  background-color: #FF8361;
+}
+.index-name-color6 {
+  color:#333333;
+  background-color: #DBB6AC;
+}
+.index-name-color7 {
+  color:#333333;
+  background-color: #DCDCDC;
+}
+.index-name-color8 {
+  color:#333333;
+  background-color: #F0DC5A;
+}
+</style>
